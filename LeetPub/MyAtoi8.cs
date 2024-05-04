ca85abbefec135561540448180fe37be42ce8f29
oguzal <oguzaltuncu@gmail.com>
2024-05-04 13:48:55 -0400

    Added lc # 8

---
+++ LeetPub/MyAtoi8.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetPub
+{
+    public  class MyAtoi8
+    {
+        public int myAtoi(string s)
+        {
+            s = s.Trim();
+
+            // Handle empty string, "+" or "-" only cases
+            if (string.IsNullOrEmpty(s) || s == "+" || s == "-")
+            {
+                return 0;
+            }
+
+            bool isNegative = s[0] == '-';
+            bool isPositive = s[0] == '+';
+
+            int startIndex = isNegative || isPositive ? 1 : 0;
+            StringBuilder numberBuilder = new StringBuilder();
+
+            for (int i = startIndex; i < s.Length; i++)
+            {
+                char ch = s[i];
+                if (char.IsDigit(ch))
+                {
+                    numberBuilder.Append(ch);
+                }
+                else
+                {
+                    if (numberBuilder.Length == 0)
+                    {
+                        return 0;
+                    }
+                    break;
+                }
+            }
+
+            string numberString = numberBuilder.ToString();
+
+            try
+            {
+                int result = int.Parse(numberString);
+                return isNegative ? -result : result;
+            }
+            catch (FormatException)
+            {
+                return isNegative ? int.MinValue : int.MaxValue;
+            }
+        }
+
+    }
+}
---
+++ LeetPubTest/MyAtoi8Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetPub;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetPub.Tests
+{
+    [TestClass()]
+    public class MyAtoi8Tests
+    {
+        [TestMethod()]
+        public void myAtoiTest()
+        {
+            var cl = new MyAtoi8();
+            Assert.AreEqual(0, cl.myAtoi("+"));
+            Assert.AreEqual(-1233, cl.myAtoi("-1233"));
+            Assert.AreEqual(57575, cl.myAtoi("+57575"));
+        }
+    }
+}
